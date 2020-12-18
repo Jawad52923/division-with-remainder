cd578761389a33dac84ca36d4c2e86084ade4168
Your Name <you@example.com>
2020-12-18 20:38:52 +0500

    first commit

---
+++ division with remainder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace division_with_remainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int divident, divisor;
+            Console.Write("Enter the divident :");
+            divident = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter the divisor :");
+            divisor = Convert.ToInt32(Console.ReadLine());
+            //divisor = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Quotient: " + divident / divisor);
+            Console.WriteLine("Remainder: " + divident % divisor);
+            Console.ReadLine();
+        }
+
+    }
+}
